82901034d01be3325585fdf2f7e49313c17e261a
JonatasAlves9 <alvesjonatas99@gmail.com>
2021-08-28 13:18:43 -0300

    feat: add styled-components in project

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container, Text } from "./styles";
+
+export const Dashboard = () => {
+  return (
+    <Container>
+      <Text>Dashboard</Text>
+    </Container>
+  );
+};
