0befaf6e12e4d51109669c56d50bc5d557e3fdbd
JonatasAlves9 <alvesjonatas99@gmail.com>
2021-08-31 18:23:22 -0300

    feat: add header of screen Register

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Dashboard } from "./src/screens/Dashboard";
+import { Register } from "./src/screens/Register";
 import { ThemeProvider } from "styled-components/native";
 import theme from "./src/global/styles/theme";
 import AppLoading from "expo-app-loading";
@@ -24,7 +24,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <Register />
     </ThemeProvider>
   );
 }
