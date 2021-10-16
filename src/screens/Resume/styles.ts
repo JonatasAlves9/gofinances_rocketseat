26c1268e41beef637326a21be3a3dbde9e2050a7
JonatasAlves9 <alvesjonatas99@gmail.com>
2021-10-16 15:43:34 -0300

    feat: adjustments in chart

--- src/screens/Resume/styles.ts
+++ src/screens/Resume/styles.ts
@@ -2,30 +2,32 @@ import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
-    flex: 1;
-    background-color: ${(props) => props.theme.colors.background};
+  flex: 1;
+  background-color: ${(props) => props.theme.colors.background};
 `;
 
 export const Header = styled.View`
-    background-color: ${({theme}) => theme.colors.primary};
-    width: 100%;
-    height: ${RFValue(113)}px;
-    align-items: center;
-    justify-content: flex-end;
-    padding-bottom: 19px;
+  background-color: ${({ theme }) => theme.colors.primary};
+  width: 100%;
+  height: ${RFValue(113)}px;
+  align-items: center;
+  justify-content: flex-end;
+  padding-bottom: 19px;
 `;
 
 export const Title = styled.Text`
-    font-family:  ${({theme}) => theme.fonts.regular};
-    color: ${({theme}) => theme.colors.shape};
-    font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+  color: ${({ theme }) => theme.colors.shape};
+  font-size: ${RFValue(18)}px;
 `;
 
 export const Content = styled.ScrollView.attrs({
-    contentContainerStyle: {flex: 1, padding: 24}
-})``;
+  contentContainerStyle: { padding: 24 },
+})`
+  flex: 1;
+`;
 
 export const ChartContainer = styled.View`
-    width: 100%;
-    align-items: center;
+  width: 100%;
+  align-items: center;
 `;
