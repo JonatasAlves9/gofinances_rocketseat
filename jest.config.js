a44b8bb7802251a176e155c40ad0e28c12647611
JonatasAlves9 <alvesjonatas99@gmail.com>
2021-10-21 20:42:39 -0300

    feat: adjustment in jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
 module.exports={
     preset: "jest-expo",
+    testPathIgnorePatterns: [
+        '/node_modules/',
+        '/android',
+        '/ios'
+    ],
     setupFilesAfterEnv: [
         "@testing-library/jest-native/extend-expect"
     ],
